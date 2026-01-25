50f51108dc782db76cc35428118efbf45860d83c
Rouven <rouven.raudzus@gmail.com>
2026-01-25 16:11:01 +0100

    Enhance CMake configuration and version handling in easybind module
    - Introduced an option to skip installation of easybind artifacts, improving flexibility in build configurations.
    - Updated CMakeLists.txt to conditionally install targets and directories based on the new option.
    - Enhanced the Python interface to include a new function for retrieving C++ version information alongside the existing version retrieval logic.

--- src_py/pymergetic/easybind/__project__.py
+++ src_py/pymergetic/easybind/__project__.py
@@ -1,14 +1,10 @@
-from pymergetic.easybind.version import get_version
+from pymergetic.easybind.version import get_version as _get_version, get_version_cpp as _get_version_cpp
 
 
 __package_name__ = "pymergetic-easybind"
-__version__ = get_version(__package_name__, root="../../..", relative_to=__file__)
-
-try:
-    from pymergetic.easybind import __cpp__
-    __version_cpp__ = __cpp__.__version__
-except Exception:
-    __version_cpp__ = "0.0.0"
+__package_module__ = "pymergetic.easybind"
+__version__ = _get_version(__package_name__, root="../../..", relative_to=__file__)
+__version_cpp__ = _get_version_cpp(__package_module__)
 
 
-__all__ = ["__package_name__", "get_version", "__version__", "__version_cpp__"]
+__all__ = ["__package_name__", "__package_module__", "__version__", "__version_cpp__"]
